3a91e4b355d28777917887bae6074c09946106e3
Ardian Pramudya Alphita <ardianpramudya81@gmail.com>
2024-10-27 07:11:44 +0700

    Initial Commit

---
+++ src/main/kotlin/com/bca/leave_request_webflux/LeaveRequestWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.bca.leave_request_webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeaveRequestWebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeaveRequestWebfluxApplication>(*args)
+}
---
+++ src/test/kotlin/com/bca/leave_request_webflux/LeaveRequestWebfluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bca.leave_request_webflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeaveRequestWebfluxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
